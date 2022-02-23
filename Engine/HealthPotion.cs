86f4c8ad051837cb24a34f04233b42dcd64a434e
Axieof <pritheev24@gmail.com>
2022-02-23 18:20:12 +0800

    Added Class derivations

--- Engine/HealthPotion.cs
+++ Engine/HealthPotion.cs
@@ -9,5 +9,10 @@ namespace Engine
     public class HealthPotion : Item
     {
         public int AmountToHeal { get; set; }
+
+        public HealthPotion(int id, string name, string namePlural, int amountToHeal) : base(id, name, namePlural)
+        {
+            AmountToHeal = amountToHeal;
+        }
     }
 }
